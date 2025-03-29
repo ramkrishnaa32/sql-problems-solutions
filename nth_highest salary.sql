1711f5e07c7cde97f5c78187345f86beb60a6aec
K Ramkrishna Achary <kramkrishnaachary@Rams-MacBook-Pro.local>
2025-03-29 22:07:49 +0530

    added few more scripts

--- nth_highest salary.sql
+++ nth_highest salary.sql
@@ -24,3 +24,16 @@ SELECT
      FROM Employee 
      ORDER BY salary DESC 
      LIMIT 1 OFFSET 2) AS ThirdHighestSalary;
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT salary FROM (
+          SELECT salary, DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
+          FROM Employee
+      ) ranked
+      WHERE rnk = N
+      LIMIT 1
+  );
+END
